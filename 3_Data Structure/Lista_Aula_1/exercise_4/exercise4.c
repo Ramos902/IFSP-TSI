b0392eba1c85137677490eb139caeeed23619ab8
Ramos <joaopedroramos1512@gmail.com>
2026-03-02 21:05:42 -0300

    Doind exercise4 Task

---
+++ 3_Data Structure/Lista_Aula_1/exercise_4/exercise4.c
@@ -0,0 +1,40 @@
+int EvenNumbers(int vec[], int vecLenght)
+{
+    int EvenCounter = 0;
+    for (int i = 0; i < vecLenght; i++)
+    {
+        if (vec[i] % 2 == 0)
+        {
+            EvenCounter++;
+        }
+    }
+    return EvenCounter;
+}
+int OddNumbers(int vec[], int vecLengh)
+{
+    int EvenCounter = 0;
+    for (int i = 0; i < vecLengh; i++)
+    {
+        if (!(vec[i] % 2 == 0))
+        {
+            EvenCounter++;
+        }
+    }
+    return EvenCounter;
+}
+
+int main()
+{
+    int vector[] = {45, 54, 76, 32, 12};
+    int vectorLenght = sizeof(vector) / sizeof(vector[0]);
+    int EvenNumberCount = EvenNumbers(vector, vectorLenght);
+    int OddNumberCount = OddNumbers(vector, vectorLenght);
+
+    for (int i = 0; i < vectorLenght; i++)
+    {
+        printf("Number %d: %d\n", i, vector[i]);
+    }
+
+    printf("\nOdd Numbers: %d", OddNumberCount);
+    printf("\nEven Numbers: %d", EvenNumberCount);
+}
